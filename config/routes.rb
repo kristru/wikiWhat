c4c6117f171bed3ab8ce87e93d666b372ba2303c
kristru <krissafuentes@gmail.com>
2018-08-21 19:39:33 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    confirmations: 'confirmations',
+    sessions: 'users/sessions'
+  }
+
   get 'welcome/index'
 
   get 'welcome/about'
