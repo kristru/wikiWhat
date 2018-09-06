ea9d53f4e1bd135b00fc2aa8d28f756563b4cdcb
kristru <krissafuentes@gmail.com>
2018-09-06 13:19:07 -0600

    remove require from application.rb and add to seeds.rb - create user and wikis seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+#Create Fake Users
+10.times do
+    User.create!(
+        email: Faker::Internet.unique.email
+        password: "password"
+    )
+    end
+    user = User.all
+
+#create Wikis
+10.times do
+    Wiki.create!(
+        title: Faker::Buffy.episode
+        body: Faker::Buffy.quote
+    )
+
+puts "Seeds Finished"
+puts "#{User.count} users created"
+puts "#{Wikis.count} wikis created"    
